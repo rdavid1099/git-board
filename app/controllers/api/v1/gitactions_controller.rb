0499a70d23523af5b3c1a233bb9794abbc222cda
Ryan Workman <rdavid1099@gmail.com>
2016-12-19 10:41:38 -0700

    Fix guard statement for initial ping"

--- app/controllers/api/v1/gitactions_controller.rb
+++ app/controllers/api/v1/gitactions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GitactionsController < ApplicationController
   def create
-    Commit.save_data(params[:commits])
+    Commit.save_data(params[:commits]) unless params[:commits].nil?
     render json: {
       status: 200,
       message: "Successfully created commits.",
